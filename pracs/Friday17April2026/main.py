c66560ac03579187d7c49df2996c0c16643cfb44
Blaze Bosch <blazebosch2@gmail.com>
2026-04-17 10:57:39 +0200

    Clean up main.py by removing comments
    Removed unnecessary comments and cleaned up code.

---
+++ pracs/Friday17April2026/main.py
@@ -0,0 +1,16 @@
+from db_connection import connect_db
+def test_connection():
+conn = connect_db()
+    if conn:
+        print("Connected successfully!")
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT * FROM tblProducts")
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            print(row)
+        
+        conn.close()
+    else:
+        print("Connection failed.")
