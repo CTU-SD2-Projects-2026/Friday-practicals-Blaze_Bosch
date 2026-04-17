a26286fbf896e765f23f767a62333aaad61e3fed
Blaze Bosch <blazebosch2@gmail.com>
2026-04-17 10:56:59 +0200

    Refactor: Move test_connection to main.py
    Removed test_connection function and related code from db_connection.py, as it should be in main.py.

--- pracs/Friday17April2026/db_connection.py
+++ pracs/Friday17April2026/db_connection.py
@@ -13,26 +13,7 @@ def connect_db():
         print("Connection failed:", e)
         return None
 
-#####################################
 
-#The code below should be in main.py
-
-from db_connection import connect_db
-def test_connection():
-conn = connect_db()
-    if conn:
-        print("Connected successfully!")
-        cursor = conn.cursor()
-        
-        cursor.execute("SELECT * FROM tblProducts")
-        rows = cursor.fetchall()
-        
-        for row in rows:
-            print(row)
-        
-        conn.close()
-    else:
-        print("Connection failed.")
 
 if name == "main":
 test_connection()
